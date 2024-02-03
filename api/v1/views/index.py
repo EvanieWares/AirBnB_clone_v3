3fe76f2399b799e097eae3d690ea091f759dfd7a
evaniewares <chisopsyelera@gmail.com>
2024-02-03 18:08:53 +0200

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,8 @@
 # api/v1/views/index.py
 """Creates views routes"""
 from flask import jsonify
+from models import *
+from models import storage
 from api.v1.views import app_views
 
 
@@ -9,3 +11,22 @@ from api.v1.views import app_views
 def status():
     """Returns a JSON status 'OK'"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns a JSON status 'OK'"""
+    amenities = storage.count('Amenity')
+    cities = storage.count('City')
+    places = storage.count('Place')
+    reviews = storage.count('Review')
+    states = storage.count('State')
+    users = storage.count('User')
+    return jsonify({
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users
+        })
