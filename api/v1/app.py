42b9e4feaede628ae7ea351a2f24cb553adb760d
evaniewares <chisopsyelera@gmail.com>
2024-02-04 17:30:51 +0200

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,12 @@
 """Create the instance of the Flask"""
 import os
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
